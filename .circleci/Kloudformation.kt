aed60086d3d037cfc1bee628026144c3e571f67f
Chris Barbour <chris.barbour03@gmail.com>
2018-12-01 14:54:47 +0000

    attempting to put resources in eu-west-1 again

--- .circleci/Kloudformation.kt
+++ .circleci/Kloudformation.kt
@@ -1,73 +1,113 @@
 import io.kloudformation.KloudFormation
 import io.kloudformation.StackBuilder
 import io.kloudformation.Value
+import io.kloudformation.function.ImportValue
 import io.kloudformation.function.plus
 import io.kloudformation.model.KloudFormationTemplate.Builder.Companion.awsRegion
+import io.kloudformation.model.Output
 import io.kloudformation.model.iam.Resource
 import io.kloudformation.model.iam.action
 import io.kloudformation.model.iam.policyDocument
 import io.kloudformation.property.certificatemanager.certificate.DomainValidationOption
-import io.kloudformation.property.cloudfront.cloudfrontoriginaccessidentity.CloudFrontOriginAccessIdentityConfig
 import io.kloudformation.property.cloudfront.distribution.*
+import io.kloudformation.resource.certificatemanager.Certificate
 import io.kloudformation.resource.certificatemanager.certificate
-import io.kloudformation.resource.cloudfront.cloudFrontOriginAccessIdentity
+import io.kloudformation.resource.cloudfront.Distribution
 import io.kloudformation.resource.cloudfront.distribution
+import io.kloudformation.resource.s3.Bucket
+import io.kloudformation.resource.s3.BucketPolicy
 import io.kloudformation.resource.s3.bucket
 import io.kloudformation.resource.s3.bucketPolicy
 
+enum class CertificationValidationMethod{ EMAIL, DNS }
+enum class SslSupportMethod(val value: String){ SNI("sni-only"), VIP("vip") }
+enum class HttpMethod(val value: String){ HTTP1_1("http1.1"), HTTP2("http2") }
+enum class CloudfrontPriceClass(val value: String){ _100("PriceClass_100"), _200("PriceClass_200"), ALL("PriceClass_ALL") }
+
+fun KloudFormation.certificate(
+        domainName: String,
+        certificateValidationMethod: CertificationValidationMethod = CertificationValidationMethod.DNS,
+        certificateBuilder: Certificate.Builder.() -> Certificate.Builder = {this}) = certificate(+"www.$domainName"){
+        subjectAlternativeNames(listOf(+domainName))
+        domainValidationOptions(listOf(DomainValidationOption(
+                domainName = +domainName,
+                validationDomain = +domainName
+        )))
+        validationMethod(certificateValidationMethod.name)
+        certificateBuilder()
+    }
+
+fun KloudFormation.s3Website(
+        domainName: String,
+        indexDocument: String = "index.html",
+        errorDocument: String = indexDocument,
+        bucketName: String? = null,
+        certificateValidationMethod: CertificationValidationMethod = CertificationValidationMethod.DNS,
+        certificateBuilder: Certificate.Builder.() -> Certificate.Builder = {this},
+        certificateReference: Value<String> = certificate(domainName,certificateValidationMethod, certificateBuilder).ref(),
+        bucketBuilder: Bucket.Builder.()-> Bucket.Builder = {this},
+        bucket: Bucket = bucket {
+            accessControl(+"PublicRead")
+            if(bucketName != null) bucketName(bucketName)
+            websiteConfiguration {
+                indexDocument(indexDocument)
+                errorDocument(errorDocument)
+            }
+            bucketBuilder()
+        },
+        bucketPolicy: BucketPolicy = bucketPolicy(
+                bucket = bucket.ref(),
+                policyDocument = policyDocument {
+                    statement(
+                            action = action("s3:GetObject"),
+                            resource = Resource(listOf(+"arn:aws:s3:::" + bucket.ref() + "/*"))
+                    ) { allPrincipals() }
+                }
+        ),
+        origin: Origin = Origin(
+                id = +"s3Origin",
+                domainName = bucket.ref() + +".s3-website-" + awsRegion + +".amazonaws.com",
+                customOriginConfig = CustomOriginConfig(
+                        originProtocolPolicy = +"http-only"
+                )
+        ),
+        sslSupportMethod: SslSupportMethod = SslSupportMethod.SNI,
+        httpMethod: HttpMethod = HttpMethod.HTTP2,
+        priceClass: CloudfrontPriceClass = CloudfrontPriceClass._200,
+        defaultCacheBehavior: DefaultCacheBehavior = DefaultCacheBehavior(
+                allowedMethods = +listOf(+"GET", +"HEAD", +"OPTIONS"),
+                forwardedValues = ForwardedValues(queryString = +true),
+                targetOriginId = origin.id,
+                viewerProtocolPolicy = +"allow-all"
+        ),
+        distributionConfig: DistributionConfig = DistributionConfig(
+                origins = listOf(origin),
+                enabled = +true,
+                aliases = +listOf(+"www.$domainName", +domainName),
+                defaultCacheBehavior = defaultCacheBehavior,
+                defaultRootObject = +indexDocument,
+                priceClass = +priceClass.value,
+                httpVersion = +httpMethod.value,
+                viewerCertificate = ViewerCertificate(acmCertificateArn = certificateReference, sslSupportMethod = +sslSupportMethod.value)
+        ),
+        distributionBuilder: Distribution.Builder.() -> Distribution.Builder = {this},
+        distribution: Distribution = distribution(
+                distributionConfig = distributionConfig
+        ){ distributionBuilder() }
+) {}
+
+val certificateVariable = "KloudsCertificate"
+
+class CertInUsEast1: StackBuilder{
+    override fun KloudFormation.create() {
+        val certificate = certificate("klouds.io")
+        outputs(
+                certificateVariable to Output(certificate.ref(), export = Output.Export(+certificateVariable))
+        )
+    }
+}
 class Kloudformation: StackBuilder{
     override fun KloudFormation.create() {
-        val index = "index.html"
-        val domainName = "klouds.io"
-        val certificate = certificate(+"www.$domainName"){
-            subjectAlternativeNames(listOf(+domainName))
-            domainValidationOptions(listOf(DomainValidationOption(
-                    domainName = +domainName,
-                    validationDomain = +domainName
-            )))
-            validationMethod("DNS")
-        }
-        bucket {
-            accessControl(+"PublicRead")
-            websiteConfiguration {
-                indexDocument(index)
-                errorDocument(index)
-            }
-        }.also { bucket ->
-            bucketPolicy(
-                    bucket = bucket.ref(),
-                    policyDocument = policyDocument {
-                        statement(
-                                action = action("s3:GetObject"),
-                                resource = Resource(listOf(+"arn:aws:s3:::" + bucket.ref() + "/*"))
-                        ) { allPrincipals() }
-                    }
-            )
-            val origin = Origin(
-                    id = +"s3Origin",
-                    domainName = bucket.ref() + +".s3-website-" + awsRegion + +".amazonaws.com",
-                    customOriginConfig = CustomOriginConfig(
-                            originProtocolPolicy = +"http-only"
-                    )
-            )
-            distribution(
-                    DistributionConfig(
-                            origins = listOf(origin),
-                            enabled = +true,
-                            aliases = +listOf(+"www.$domainName", +domainName),
-                            defaultCacheBehavior = DefaultCacheBehavior(
-                                    allowedMethods = +listOf(+"GET", +"HEAD", +"OPTIONS"),
-                                    forwardedValues = ForwardedValues(queryString = +true),
-                                    targetOriginId = origin.id,
-                                    viewerProtocolPolicy = +"allow-all"
-                            ),
-                            defaultRootObject = +"index.html",
-                            priceClass = +"PriceClass_200",
-                            httpVersion = +"http2",
-                            viewerCertificate = ViewerCertificate(acmCertificateArn = certificate.ref(), sslSupportMethod = +"sni-only")
-                    )
-            )
-        }
-
+        s3Website("klouds.io", bucketName = "kloudformation-website",certificateReference = ImportValue(+certificateVariable))
     }
 }
